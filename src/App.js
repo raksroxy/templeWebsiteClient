4595ab987df1c5ee8f98a6981c68aa00ce5e70c2
raksroxy <rakshithshetty962@gmail.com>
2022-01-08 17:44:46 +0530

    Inital commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hello world</h1>
+     <p>Hello i from the </p>
+    </div>
+  );
+}
+
+export default App;
